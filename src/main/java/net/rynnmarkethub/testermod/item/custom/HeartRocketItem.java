2c48d7cf7b39ce6d40195a466728344dcc0f62e0
Ryan Canino <ryankcanino@gmail.com>
2026-02-06 19:15:59 -0800

    Created explosion for heart rocket

--- src/main/java/net/rynnmarkethub/testermod/item/custom/HeartRocketItem.java
+++ src/main/java/net/rynnmarkethub/testermod/item/custom/HeartRocketItem.java
@@ -1,15 +1,23 @@
 package net.rynnmarkethub.testermod.item.custom;
 
+import it.unimi.dsi.fastutil.ints.IntArrayList;
 import net.minecraft.core.Direction;
+import net.minecraft.core.component.DataComponents;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.projectile.FireworkRocketEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.FireworkExplosion;
+import net.minecraft.world.item.component.Fireworks;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.rynnmarkethub.testermod.item.ModItems;
+
+import java.util.List;
 
 public class HeartRocketItem extends Item {
     public HeartRocketItem(Properties properties){
@@ -20,10 +28,17 @@ public class HeartRocketItem extends Item {
     public InteractionResult useOn(UseOnContext context) {
         Level level = context.getLevel();
         if (!level.isClientSide) {
-            ItemStack itemstack = context.getItemInHand();
-            CompoundTag hearttag = new CompoundTag();
-            ListTag explosionsList = new ListTag();
-            CompoundTag explosion = new CompoundTag();
+            ItemStack heartRocketItem = new ItemStack(ModItems.HEART_ROCKET.get());
+            FireworkExplosion customExplosion = new FireworkExplosion(
+                    FireworkExplosion.Shape.LARGE_BALL,
+                    IntArrayList.of(0x0000FF, 0x008000),
+                    IntArrayList.of(0xFFC0CB),
+                    true,
+                    true
+            );
+
+            List<FireworkExplosion> explosions = List.of(customExplosion);
+            heartRocketItem.set(DataComponents.FIREWORKS, new Fireworks(2, explosions));
             Vec3 vec3 = context.getClickLocation();
             Direction direction = context.getClickedFace();
 
@@ -34,10 +49,10 @@ public class HeartRocketItem extends Item {
                     vec3.x + (double)direction.getStepX() * 0.15,
                     vec3.y + (double)direction.getStepY() * 0.15,
                     vec3.z + (double)direction.getStepZ() * 0.15,
-                    itemstack
+                    heartRocketItem
             );
             level.addFreshEntity(fireworkrocketentity);
-            itemstack.shrink(1);
+            heartRocketItem.shrink(1);
         }
 
         return InteractionResult.sidedSuccess(level.isClientSide);
